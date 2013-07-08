7c779ed32f9354dcc4bb46dbd460a8d4fd01b785
Logan Sears <lsears322@gmail.com>
2013-07-08 13:43:53 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyCal::Application.config.secret_token = '3cfb17b4f2e1193f1ae2f8c3e36afc25c12077d3ac6ed366fea1f5200e4659b08ef5a0342b8599524a5864d248d8b90e2d93c4620e5f29b159fa2e27f0b27657'
